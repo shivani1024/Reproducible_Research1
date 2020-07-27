615ff8383150953771a498491473612b06f1f81f
shivani1024 <51295296+shivani1024@users.noreply.github.com>
2020-07-27 12:01:47 +0530

    Create Average_steps.R

---
+++ Average_steps.R
@@ -0,0 +1,2 @@
+steps_by_interval <- aggregate(steps ~ interval, data, mean)
+plot(steps_by_interval$interval,steps_by_interval$steps, type="l", xlab="Interval", ylab="Number of Steps",main="Average Number of Steps per Day by Interval")
