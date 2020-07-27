575857972e7d85285a34aaa3f137a9a60d782454
shivani1024 <51295296+shivani1024@users.noreply.github.com>
2020-07-27 12:22:21 +0530

    Create 7_MissingDataHist.R

---
+++ 7_MissingDataHist.R
@@ -0,0 +1,7 @@
+# Make a histogram of the total number of steps taken each day and Calculate and report the mean and median total number of steps taken per day
+
+StepsTotalUnion <- aggregate(steps ~ date, data = new_activity, sum, na.rm = TRUE)
+hist(StepsTotalUnion$steps, main = paste("Total Steps Each Day"), col="blue", xlab="Number of Steps")
+#Create Histogram to show difference. 
+hist(steps_by_day$steps, main = paste("Total Steps Each Day"), col="green", xlab="Number of Steps", add=T)
+legend("topright", c("Imputed", "Non-imputed"), col=c("blue", "green"), lwd=10)
